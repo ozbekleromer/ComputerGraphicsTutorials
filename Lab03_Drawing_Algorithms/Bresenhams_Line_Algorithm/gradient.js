0f9e67b81dbdd256ca67dae291b5945e0f56198f
ozbekleromer <45530887+ozbekleromer@users.noreply.github.com>
2020-11-03 21:28:40 +0100

    Update gradient.js

--- Lab03_Drawing_Algorithms/Bresenhams_Line_Algorithm/gradient.js
+++ Lab03_Drawing_Algorithms/Bresenhams_Line_Algorithm/gradient.js
@@ -33,25 +33,30 @@ function mouseReleased() {
 }
 
 function set_pixel(x0, y0, a) {
+  // for each pixel in the canvas calculate colors
   for (x = 0; x < 512; x++) {
     for (y = 0; y < 512; y++) {
+      //Calculate pixel position in matrix
       idx = (y * 512 + x) * 4;
+      //Calculate distance between pixel and line
       dxy = a * (x - x0) - (y - y0);
-      
-      if(dxy < 0) {
+
+      //Above the line increase green value
+      if (dxy < 0) {
         pixels[idx] = -dxy;
         pixels[idx + 1] = 0;
-      } else {
+      } else { //Under the line increase red value
         pixels[idx] = 0;
         pixels[idx + 1] = dxy;
       }
       pixels[idx + 2] = 0;
       pixels[idx + 3] = 255;
     }
-  }  
+  }
 }
 
 function draw_line() {
+  // Make always x0 smaller one
   if (x0 > x1) {
     t = x0;
     x0 = x1;
@@ -62,6 +67,7 @@ function draw_line() {
     y1 = t;
   }
 
+  //Calculate variables
   dx = x1 - x0;
   dy = y1 - y0;
 
